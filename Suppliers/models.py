721294e8cba756f1a745f19dde05ef1c6ce41fdd
Cezar Roberto Poeta <cezarrobertopoeta@gmail.com>
2021-05-26 09:10:22 -0300

    Update models.py
    Update Models using get_user_model

--- Suppliers/models.py
+++ Suppliers/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 
 class Base(models.Model):
+    userApp = models.ForeignKey(get_user_model(), verbose_name="User", on_delete=models.CASCADE)
     created = models.DateField(db_column='created', auto_now_add=True)
     updated = models.DateField(db_column='updated', auto_now=True)
     active = models.BooleanField('Ativo', default=True)
