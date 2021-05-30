a997182c4a97b10884a44e4b17ad34342cadb32f
Cezar Roberto Poeta <cezarrobertopoeta@gmail.com>
2021-05-30 15:07:46 -0300

    Updated
    UPdated

--- Suppliers/views.py
+++ Suppliers/views.py
@@ -1,13 +1,13 @@
-from django.forms import forms
+#from django.forms import forms
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import TemplateView
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 
-from collections import UserList, UserString
-from django.http import request
-from django.views.generic.base import ContextMixin
-from django.shortcuts import redirect
+#from collections import UserList, UserString
+#from django.http import request
+#from django.views.generic.base import ContextMixin
 
 
 from .models import Tbbeneficiarios
@@ -25,7 +25,7 @@ class IndexListView(ListView):
     model = Tbbeneficiarios
     template_name = 'flstbeneficiarios.html'
     paginate_by = 15
-    ordering = 'id'
+    ordering = '-id'
     queryset = Tbbeneficiarios.objects.all()
     context_object_name = 'supliers' 
 
@@ -34,30 +34,34 @@ class CreateBeneficiarioView(CreateView):
     model = Tbbeneficiarios
     template_name = 'fBeneficiarios.html'
     fields = ['nome',]
-    success_url = reverse_lazy('index')
-
-
+    success_url = reverse_lazy('lst_beneficiarios')
     """
     Esta lógica está dentro do CreateView, é herança do BaseCreateView, Trouxe para o código e fiz as alterações
     Acrescentando o tratamento do request.user
     """
     def get(self, request, *args, **kwargs):       
         if str(request.user) == 'AnonymousUser':
-            return redirect('index')
+            return redirect('lst_beneficiarios')
         else:
-            print(dir(request))
-
             self.object = None
             return super().get(request, *args, **kwargs)
-    
+    """
+    Esta lógica me deu acesso aos dados do formulário para eu acrescentar e/ou alterar os dados informados pelo usuário
+    """
+    def form_valid(self, form):
+#        print(f'Nome: {form.instance.nome}') # Mostra o dado informado pelo usuário no formulário
+        form.instance.useradm = self.request.user
+        return super().form_valid(form)
+
+
 class UpdateBeneficiarioView(UpdateView):
     model = Tbbeneficiarios
     template_name = 'fBeneficiarios.html'
     fields = ['nome',]
-    success_url = reverse_lazy('index')  
+    success_url = reverse_lazy('lst_beneficiarios')  
 
 
 class DeleteBeneficiarioView(DeleteView):
     model = Tbbeneficiarios
     template_name = 'fdel-Beneficiarios.html'
-    success_url = reverse_lazy('index')  
+    success_url = reverse_lazy('lst_beneficiarios')  
